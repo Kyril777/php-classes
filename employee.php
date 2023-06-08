46980125fea23d42bb74861fb18de0c4bad54ca2
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-06-07 21:41:33 -0400

    Update employee.php

--- employee.php
+++ employee.php
@@ -1,12 +1,14 @@
 <?php
-/* PHP class for company employees. */
+/**
+ * PHP class for company employees.
+ */
 class Employee
 {
   private $first_name;
   private $last_name;
-  private id;
+  private $id;
  
-  public function __construct($first_name, $last_name, $age)
+  public function __construct($first_name, $last_name, $id)
   {
     $this->first_name = $first_name;
     $this->last_name = $last_name;
@@ -25,3 +27,15 @@ class Employee
     return $this->id;
   }
 }
+
+// First class instantiation.
+$objEmployeeOne = new Employee('Wes', 'Winston', 123456);
+echo $objEmployeeOne->getFirstName() .PHP_EOL; // print 'Wes' 
+echo $objEmployeeOne->getLastName() .PHP_EOL; // prints 'Winston' 
+echo $objEmployeeOne->getId() .PHP_EOL; // prints '123456'
+
+// Second class instantiaion.
+$objEmployeeTwo = new Employee('Declan', 'George', 246800);
+echo $objEmployeeTwo->getFirstName() .PHP_EOL; // prints 'Declan' 
+echo $objEmployeeTwo->getLastName() .PHP_EOL; // prints 'George' 
+echo $objEmployeeTwo->getId() .PHP_EOL; // prints '246800'
