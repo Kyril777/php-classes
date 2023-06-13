db57d3a43bd5548acddfee0f8393d16cf8a18078
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-06-12 23:56:22 -0400

    Update BankAccount.php

--- BankAccount.php
+++ BankAccount.php
@@ -23,10 +23,10 @@ class BankAccount {
   }
   
   public function __destruct(){
-  	echo "The {$this->last_name} account numbered {$this->number} with a {$this->type} type has a balance of {$this->balance}.";
+  	echo "The {$this->last_name} account numbered {$this->number} with a {$this->type} type has a balance of {$this->balance} as of " .  date("F d, Y h:i:s A") . ".";
   }
 }
 
-$smithersAccount = new BankAccount("Smithers", 12345, "checking", 100.01); // The Smithers account numbered 12345 with a checking type has a balance of 100.01.
-$smithersAccount -> deposit(50.00); // The Smithers account numbered 12345 with a checking type has a balance of 150.01.
-$smithersAccount -> withdraw(25.00); // // The Smithers account numbered 12345 with a checking type has a balance of 125.01.
+$smithersAccount = new BankAccount("Smithers", 12345, "checking", 100.01); // The Smithers account numbered 12345 with a checking type has a balance of 100.01 as of June 12, 2023 05:53:36 AM.
+$smithersAccount -> deposit(50.00); // The Smithers account numbered 12345 with a checking type has a balance of 150.01 as of June 12, 2023 05:53:56 AM.
+$smithersAccount -> withdraw(25.00); // // The Smithers account numbered 12345 with a checking type has a balance of 125.01 as of June 12, 2023 05:54:39 AM.
