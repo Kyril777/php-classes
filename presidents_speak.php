a6f46053ed18064aa986412195bd1f975293544a
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-06-14 23:22:54 -0400

    Create presidents_speak.php

---
+++ presidents_speak.php
@@ -0,0 +1,41 @@
+<?php
+/* PHP interface that extends the speak() method to classes. */
+interface President {
+  public function speak();
+}
+
+
+class FDR implements President {
+  public function speak() {
+    echo "Happiness lies in the joy of achievement and the thrill of creative effort" .PHP_EOL;
+  }
+}
+
+class JFK implements President {
+  public function speak() {
+    echo "Efforts and courage are not enough without purpose and direction." .PHP_EOL;
+  }
+}
+
+class LBJ implements President {
+  public function speak() {
+    echo "Yesterday is not ours to recover, but tomorrow is ours to win or lose." .PHP_EOL;
+  }
+}
+
+// Create a list of presidents.
+$fRoosevelt = new FDR();
+$jKennedy = new JFK();
+$lJohnson = new LBJ();
+$presidents = array($fRoosevelt, $jKennedy, $lJohnson);
+
+// Tell each president to speak
+foreach($presidents as $president) {
+  $president->speak();
+}
+
+/* 
+Happiness lies in the joy of achievement and the thrill of creative effort
+Efforts and courage are not enough without purpose and direction.
+Yesterday is not ours to recover, but tomorrow is ours to win or lose.
+*/
