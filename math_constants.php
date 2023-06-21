236de0dab3eee83a72de8e30bb95c5221bf7dcdb
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-06-20 23:02:05 -0400

    Create math_constants.php

---
+++ math_constants.php
@@ -0,0 +1,39 @@
+<?php
+/* Produce mathematical constants through polymorphism. */
+interface Constant {
+	public function printConstant();
+}
+
+class Pi implements Constant {
+  public function printConstant() {
+		return 3.14159;
+	}
+}
+
+class Tau implements Constant {
+  public function printConstant() {
+		return 6.28318;
+	}
+}
+
+class EulerNumber implements Constant {
+  public function printConstant() {
+		return 2.71828;
+	}
+}
+
+class EulerConstant implements Constant {
+  public function printConstant() {
+		return 0.57721;
+	}
+}
+
+function produce($constants) {
+	foreach ($constants as $constant) {
+		echo $constant->printConstant() .PHP_EOL;
+	}
+}
+
+$constants = [ new Pi(), new Tau(),	new EulerNumber(), new EulerConstant()];
+
+produce($constants);
