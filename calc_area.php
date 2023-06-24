fedcc7b15cb8aa015cc38968fecadb7124ca19d3
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-06-23 23:26:26 -0400

    Create calc_area.php

---
+++ calc_area.php
@@ -0,0 +1,25 @@
+<?php
+/* Create shape objects to calculate each area. */
+interface Shape{
+
+public function calculateArea(){
+
+}
+
+class Square implements Shape{}
+
+class Rectangle implements Shape{}
+
+class Triangle implements Shape{}
+
+$sqe = new Square();
+
+$sqe->calculateArea();
+
+$rct = new RectangleExmp();
+
+$rct->calculateArea();
+
+$tri = new TriangleExmp();;
+
+$tri->calculateArea();
