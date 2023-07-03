73b7b1a23bcc86f31db984976004b4eb4e9147f1
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-07-02 23:55:56 -0400

    Update collections.php

--- collections.php
+++ collections.php
@@ -4,12 +4,27 @@ class Collections {
   public $author = "";
   public $volumes = 0;
   public function message() {
-    echo "<p>The '{$this->author}' collection has {$this->volumes} volumes.</p>";
+    echo "<p>The '{$this->author}' collection has {$this->volumes} volumes.</p>" .PHP_EOL;
   }
 }
 
-$collection = new Collections();
-$collection->author = "Jane Austen";
-$collection->volumes = 9;
+$austen = new Collections();
+$austen->author = "Jane Austen";
+$austen->volumes = 9;
 
-$collection->message(); // The 'Jane Austen' collection has 9 volumes.
+$austen->message(); // The 'Jane Austen' collection has 9 volumes.
+
+// Extend the class to add edition(s).
+class Edition extends Collections {
+  public $editions = "";
+  public function message() {
+    echo "<p>The '{$this->author}' collection has {$this->volumes} volumes and is in the {$this->editions} edition(s).</p>" .PHP_EOL;
+  }
+}
+
+$britannica = new Edition();
+$britannica->author = "Wagnals Britannica";
+$britannica->volumes = 26;
+$britannica->editions = "3rd";
+ 
+$britannica->message(); // The 'Wagnals Britannica' collection has 26 volumes and is in the 3rd edition(s).
