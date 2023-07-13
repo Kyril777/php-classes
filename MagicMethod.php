360e78d0c865c6b34d8950a26fa9489e385bcf19
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-07-12 23:19:41 -0400

    Update MagicMethod.php

--- MagicMethod.php
+++ MagicMethod.php
@@ -85,3 +85,40 @@ $whitney->Whitney; // Good morning, 'Whitney'!
 
 $kate = new MagicGreeting();
 $kate->Kate; // Good morning, 'Kate'!
+
+
+
+/* A sample __construct() method to create HTML elements. */
+
+class MagicConstruct{
+	private $attributes = [];
+
+	private $tag;
+
+	public function __construct($tag){
+		$this->tag = $tag;
+	}
+
+	public function __set($name, $value){
+		$this->attributes[$name] = $value;
+	}
+
+	public function html($innerHTML = ''){
+		$html = "<{$this->tag}";
+		foreach ($this->attributes as $key => $value) {
+			$html .= ' ' . $key . '="' . $value . '"';
+		}
+		$html .= '>';
+		$html .= $innerHTML;
+		$html .= "</$this->tag>";
+
+		return $html;
+	}
+}
+
+$div = new MagicConstruct('div');
+
+$div->id = 'page';
+$div->class = 'info';
+
+echo $div->html('Inside of a div.'); // <div id="page" class="info">Inside of a div.</div>
