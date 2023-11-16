51d3a2e7f59159568bf20ccc59cbe0829512b6ad
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-11-16 18:05:36 -0500

    Update index.php

--- php-exception-error-handling/php-signup-error-handling/index.php
+++ php-exception-error-handling/php-signup-error-handling/index.php
@@ -17,5 +17,18 @@
             <input type="password" name="pwd" placeholder="Password">
             <button type="submit" name="submit">Register</button>
         </form>
+        <?php
+          $fullUrl = "http://$_SERVER[HTTP_POST]$_SERVER[REQUEST_URI]";
+
+          if (strpos($fullUrl, "signup=empty") == true) {
+            echo "<p class="error">Not all fields have been filled.</p>";
+          } elseif (strpos($fullUrl, "signup=char") == true) {
+            echo "<p clas="error">There are invalid characters.</p>";
+          } elseif (strpos($fullUrl, "signup=email") == true) {
+            echo "<p class="error">You have entered an invalid email address.</p>";
+          } elseif (strpos($fullUrl, "signup=success") == true) {
+            echo "<p class="success">Congratulations!</p>";
+          }
+        ?>
     </body>
 </html>
